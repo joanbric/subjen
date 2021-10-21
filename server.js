2a00e53f9a62158d109310f0c7037a7f1b5d4998
Glitch (glitch-hello-node) <none>
2021-10-21 18:42:39 +0000

    🏈🛍 Checkpoint ./server.js:32302306/333

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-/**
-* This is the main Node.js server script for your project
-* Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
-*/
-
 const path = require("path");
-
-// Require the fastify framework and instantiate it
 const fastify = require("fastify")({
-  // Set this to true for detailed logging:
   logger: false
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-
-// Setup our static files
 fastify.register(require("fastify-static"), {
   root: path.join(__dirname, "public"),
   prefix: "/" // optional: default '/'
