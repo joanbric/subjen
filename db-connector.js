09ac827d9b9e78dbd8bfd4e3c95717dc073dcf1f
Glitch (glitch-hello-node) <none>
2021-10-24 03:38:37 +0000

    🔔📍 Checkpoint ./db-connector.js:32302306/18 ./router.js:32302306/670

--- db-connector.js
+++ db-connector.js
@@ -2,7 +2,7 @@ const fastifyPlugin = require('fastify-plugin');
 
 async function dbConnector(fastify, options){
   fastify.register(require('fastify-mongodb'),{
-    url: 'mongodb+srv://bus.tigys.mongodb.net/myFirstDatabase" --username master'
+    url: 'mongodb+srv://bus.tigys.mongodb.net/bus" --username master'
   })
 }
 
--- router.js
+++ router.js
@@ -1,6 +1,23 @@
 async function router(fastify, options) {
+  const collection = fastify.mongo.db.collection('users')
+  
   fastify.get("/", async (request, response) => {
     return "world";
+  });
+  
+  fastify.get('/animals', async (req, reply) =>{
+    const result = await collection.find().toArray()
+    if(result.length === 0){
+      throw new Error('No documents found');
+    }
+    return result;
+  })
+  
+  
+  fastify.get('/animals/:animal', async (req, replay) => {
+    const result = await collection.findOne({'animal': req.params.animal})
+    if(!result) throw new Error('Invalid value');
+    return result;
   });
 }
 
