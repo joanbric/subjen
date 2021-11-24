e6834e0d6c71942dc82c70a7681c2f34ff78ad8d
Glitch (glitch-hello-node) <none>
2021-11-24 07:37:54 +0000

    👀🐢 Checkpoint ./source/public/index.html:32302306/10

--- source/public/js/ws/index.js
+++ source/public/js/ws/index.js
@@ -1,4 +1,30 @@
-const socket =  new WebSocket('ws://localhost:3000/')
-socket.onopen = () => {
-    socket.send("Hello!")
-}
+const socket = io();
+const btnSend = document.querySelector("#btnSend");
+const txtMsg = document.querySelector("#txtMsg");
+const btnEmit = document.querySelector("#btnEmit");
+
+
+
+
+
+
+btnSend.addEventListener("click", () => {
+    socket.send(txtMsg.value);
+});
+
+btnEmit.addEventListener('click', () => {
+  socket.emit('chat1', txtMsg.value)
+})
+socket.on("connect", () => {
+    console.log(socket.id);
+});
+
+socket.on("message", (data) => {
+    console.log("You recive: " + data);
+});
+
+socket.on("chat1", (data) => {
+    console.group("Chat 1");
+    console.log(data);
+    console.groupEnd("Chat 1");
+});
