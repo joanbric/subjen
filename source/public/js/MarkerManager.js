c161a9b2286774009be78717adff6142e6891ac2
Jhon A. Briones <joanbric@gmail.com>
2021-11-30 03:39:45 -0600

    Change log.error to new Error

--- source/public/js/MarkerManager.js
+++ source/public/js/MarkerManager.js
@@ -25,7 +25,7 @@ function getMe() {
 }
 
 function newMarker(options = {}) {
-    if(!_map) console.error("No have any map to create a marker")
+    if(!_map) throw new Error("No have any map to create a marker")
     let markerOptions = {
         map: _map,
         position: options.position,
